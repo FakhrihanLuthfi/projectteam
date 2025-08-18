2c163f29100ba56b1806dd46977a7a730c1b8001
FakhrihanLuthfi <fakhrihanluthfi1@gmail.com>
2025-08-18 15:02:31 +0700

    update bug

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">My Kost</a>
-      <div class="d-flex">
+      <div class="d-flex" v-if="isLoggedIn">
         <button class="btn btn-outline-primary" @click="logout">Logout</button>
       </div>
     </div>
@@ -10,13 +10,21 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 
+// ambil router
 const router = useRouter();
+
+// cek session login
+const isLoggedIn = ref(localStorage.getItem("isLoggedIn") === "true");
 
 const logout = () => {
   // hapus session login
   localStorage.removeItem("isLoggedIn");
+
+  // update state supaya button hilang
+  isLoggedIn.value = false;
 
   // redirect ke login page
   router.push({ name: "Login" });
