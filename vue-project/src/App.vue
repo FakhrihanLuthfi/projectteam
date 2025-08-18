5026b14b3dc4e7d084f0a1bb891345248392406a
iqbal <iqbalkhazanatul@gmail.com>
2025-08-18 14:02:55 +0700

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,19 +1,27 @@
 <script setup>
-import Sidebar from './components/Sidebar.vue'
-import Navbar from './components/Navbar.vue'
-import Dashboard from './views/Dashboard.vue'
+import { useRoute } from "vue-router";
+import Sidebar from "./components/Sidebar.vue";
+import Navbar from "./components/Navbar.vue";
+
+const route = useRoute();
 </script>
 
 <template>
-  <div class="d-flex">
+  <!-- kalau masih di Login -->
+  <div v-if="route.name === 'Login'">
+    <router-view />
+  </div>
+
+  <!-- kalau sudah login (Dashboard & lainnya) -->
+  <div v-else class="d-flex">
     <!-- Sidebar -->
     <Sidebar />
 
-    <!-- Main content -->
+    <!-- Main Content -->
     <div class="flex-grow-1">
       <Navbar />
       <main class="container-fluid mt-3">
-        <Dashboard />
+        <router-view />
       </main>
     </div>
   </div>
--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -3,10 +3,22 @@
     <div class="container-fluid">
       <a class="navbar-brand" href="#">My Kost</a>
       <div class="d-flex">
-        <button class="btn btn-outline-primary">Logout</button>
+        <button class="btn btn-outline-primary" @click="logout">Logout</button>
       </div>
     </div>
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const logout = () => {
+  // hapus session login
+  localStorage.removeItem("isLoggedIn");
+
+  // redirect ke login page
+  router.push({ name: "Login" });
+};
+</script>
