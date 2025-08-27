878b27253301cc1572bdedfa6abbabb28c50cb3c
Fakhrihan Luthfi <fakhrihan.luthfi@iconpln.co.id>
2025-08-27 22:25:01 +0700

    update

--- backend/kostease/src/main/java/com/example/kostease/KosteaseApplication.java
+++ backend/kostease/src/main/java/com/example/kostease/KosteaseApplication.java
@@ -1,13 +1,13 @@
-package com.example.kostku;
+package com.example.kostease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KostkuApplication {
+public class KosteaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KostkuApplication.class, args);
+		SpringApplication.run(KosteaseApplication.class, args);
 	}
 
 }
--- backend/kostease/src/test/java/com/example/kostease/KosteaseApplicationTests.java
+++ backend/kostease/src/test/java/com/example/kostease/KosteaseApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.kostku;
+package com.example.kostease;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KostkuApplicationTests {
+class KosteaseApplicationTests {
 
 	@Test
 	void contextLoads() {
